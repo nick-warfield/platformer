f1adf086fe6799d3082b68bc9fe709769293751b
Nicholas Warfield <nick@nkwarfield.io>
2021-05-06 23:13:10 -0700

    Collision Working

--- include/Game.hpp
+++ include/Game.hpp
@@ -7,6 +7,7 @@
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
 
+#include "Block.hpp"
 #include "Tetrimino.hpp"
 
 struct Game {
@@ -19,40 +20,48 @@ struct Game {
 	Tetrimino held_tetrimino;
 	std::queue<Tetrimino> next_tetriminos;
 
-	std::vector<sf::Sprite> placed_blocks;
+	std::vector<Block> placed_blocks;
 	sf::Vector2<int> place_position;
 };
 
 Game make_game(int board_width = 10, int board_height = 20) {
 	Game game;
-	game.place_position = sf::Vector2(board_width / 2, 9) * BLOCK_SCALE;
+	game.place_position = sf::Vector2(board_width / 2, 9);
 	game.player_tetrimino = make_random(game.place_position);
-	sf::Texture t;
-	t.loadFromFile("resources/block.png");
+//	sf::Texture t;
+//	t.loadFromFile("resources/block.png");
 	sf::Color wall_color = sf::Color::White;
 
 	// set walls
 	for (int i = 1; i < board_height + 10; ++i) {
-		sf::Sprite block1(t);
-		block1.setColor(wall_color);
-		block1.setPosition(0, i * BLOCK_SCALE);
+//		sf::Sprite block1(t);
+//		block1.setColor(wall_color);
+//		block1.setPosition(0, i * BLOCK_SCALE);
+//
+//		sf::Sprite block2(t);
+//		block2.setColor(wall_color);
+//		block2.setPosition(board_width * BLOCK_SCALE, i * BLOCK_SCALE);
+//
+//		game.placed_blocks.push_back(block1);
+//		game.placed_blocks.push_back(block2);
 
-		sf::Sprite block2(t);
-		block2.setColor(wall_color);
-		block2.setPosition(board_width * BLOCK_SCALE, i * BLOCK_SCALE);
-
-		game.placed_blocks.push_back(block1);
-		game.placed_blocks.push_back(block2);
+		game.placed_blocks.push_back(
+				make_block(wall_color, sf::Vector2i(0, i)));
+		game.placed_blocks.push_back(
+				make_block(wall_color, sf::Vector2i(board_width, i)));
 	}
 
 	// set floor
 	for (int i = 0; i <= board_width; ++i) {
-		sf::Sprite block(t);
-		block.setColor(wall_color);
-		block.setPosition(
-				i * BLOCK_SCALE,
-				(board_height + 10) * BLOCK_SCALE);
-		game.placed_blocks.push_back(block);
+//		sf::Sprite block(t);
+//		block.setColor(wall_color);
+//		block.setPosition(
+//				i * BLOCK_SCALE,
+//				(board_height + 10) * BLOCK_SCALE);
+//		game.placed_blocks.push_back(block);
+
+		game.placed_blocks.push_back(
+				make_block(wall_color, sf::Vector2i(i, board_height + 10)));
 	}
 
 	return game;
@@ -60,27 +69,25 @@ Game make_game(int board_width = 10, int board_height = 20) {
 
 // collision check, real simple because blocks are
 // grid and rotation locked, and the same size
-// not working, probably because sf::Sprite.position uses floats
 bool check_collision(Game& level) {
 	bool is_collision = false;;
 	for (auto b : level.player_tetrimino.blocks) {
 		for (auto p : level.placed_blocks) {
-			if (p.getPosition() == b.getPosition()) {
+			if (p.position == level.player_tetrimino.position + b.position) {
 				is_collision = true;
 			}
 		}
 	}
-	//std::cout << is_collision << std::endl;
 	return is_collision;
 }
 
 void move_player_tetrimino(Game& level, int x, int y) {
-	level.player_tetrimino.position.x += x * BLOCK_SCALE;
-	level.player_tetrimino.position.y += y * BLOCK_SCALE;
+	level.player_tetrimino.position.x += x;
+	level.player_tetrimino.position.y += y;
 
 	if (check_collision(level)) {
-		level.player_tetrimino.position.x -= x * BLOCK_SCALE;
-		level.player_tetrimino.position.y -= y * BLOCK_SCALE;
+		level.player_tetrimino.position.x -= x;
+		level.player_tetrimino.position.y -= y;
 	}
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
-//#include "Game.hpp"
+#include "Game.hpp"
 #include "Tetrimino.hpp"
 #include "Block.hpp"
 
@@ -12,38 +12,15 @@ int main()
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Tetris");
 
-    // Load a sprite to display
-//    sf::Texture texture;
-//    if (!texture.loadFromFile("./resources/block.png"))
-//        return EXIT_FAILURE;
-//    sf::Sprite sprite(texture);
-
     // Load a music to play
 //    sf::Music music;
 //    if (!music.openFromFile("resources/Shapeforms/Dystopia – Ambience and Drone Preview/AUDIO/AMBIENCE_SIGNAL_LOOP.wav"))
 //        return EXIT_FAILURE;
 //    music.play();
 
-	std::vector<Tetrimino> tetri;
-	tetri.push_back(make_tetrimino(sf::Vector2<int>(400, 300), 0));
-	tetri.push_back(make_tetrimino(sf::Vector2<int>(800, 300), 1));
-	tetri.push_back(make_tetrimino(sf::Vector2<int>(1200, 300), 2));
-	tetri.push_back(make_tetrimino(sf::Vector2<int>(300, 600), 3));
-	tetri.push_back(make_tetrimino(sf::Vector2<int>(600, 600), 4));
-	tetri.push_back(make_tetrimino(sf::Vector2<int>(900, 600), 5));
-	tetri.push_back(make_tetrimino(sf::Vector2<int>(1200, 600), 6));
-
 	sf::Clock timer;
-
-//	Game game = make_game();
-
+	Game game = make_game();
 	sf::RenderStates states = sf::RenderStates::Default;
-	Block b1 = make_block(sf::Color::Cyan);
-	Block b2 = make_block(sf::Color::Green, sf::Vector2i(1, 1));
-	Block b3 = make_block(sf::Color::Magenta, sf::Vector2i(2, 2));
-	Block b4 = make_block(sf::Color::Red, sf::Vector2i(3, 3));
-
-	Tetrimino t = make_tetrimino(sf::Vector2i(10, 4), 0);
 
     // Start the game loop
     while (window.isOpen())
@@ -59,32 +36,20 @@ int main()
 		
 		// rotate blocks every 3/4th second
 		if (timer.getElapsedTime().asSeconds() > 0.75f) {
-			for (auto &t : tetri) {
-				rotate(t);
-			}
-			rotate(t);
 			timer.restart();
 		}
 
-//		run(game);
+		run(game);
 
         // Clear screen
         window.clear();
 
         // Draw the sprite
-		//for (auto t : tetri) { draw_tetrimino(t, window); }
-		//for (auto b : game.placed_blocks) { window.draw(b); }
-		//draw_tetrimino(game.player_tetrimino, window);
-		states.transform = sf::Transform::Identity;
-		draw_block(window, states, b1);
-		states.transform = sf::Transform::Identity;
-		draw_block(window, states, b2);
-		states.transform = sf::Transform::Identity;
-		draw_block(window, states, b3);
-		states.transform = sf::Transform::Identity;
-		draw_block(window, states, b4);
-
-		draw_tetrimino(window, states, t);
+		for (auto b : game.placed_blocks) {
+			states.transform = sf::Transform::Identity;
+			draw_block(window, states, b);
+		}
+		draw_tetrimino(window, states, game.player_tetrimino);
 
         // Update the window
         window.display();
