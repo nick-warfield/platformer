3ef83fbf512e3c5dddd7314cee4f6437564c5a63
Nicholas Warfield <nick@nkwarfield.io>
2021-05-06 17:49:38 -0700

    Create Game Skeleton

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,29 +1,29 @@
 #pragma once
 
 #include <vector>
+#include <queue>
 
+#include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
 
 #include "Tetrimino.hpp"
 
 struct Game {
 	int score = 0;
-
-	float down_timer = 1.0f;
-
-	// rather than bounds, I can use invisible blocks
-	int left_bound = 0 * BLOCK_SCALE;
-	int right_bound = 10 * BLOCK_SCALE;
-	int lower_bound = 0 * BLOCK_SCALE;
-	int upper_bound = 20 * BLOCK_SCALE;
+	int level = 0;
+	float fall_speed = 1.0f;
 
 	Tetrimino player_tetrimino;
 	Tetrimino held_tetrimino;
+	std::queue<Tetrimino> next_tetriminos;
+
 	std::vector<sf::Sprite> placed_blocks;
+	sf::Vector2<int> place_position;
 };
 
 Game make_game(int board_width = 10, int board_height = 20) {
 	Game game;
+	game.place_position = sf::Vector2(board_width / 2, board_height / 2);
 	sf::Texture t;
 	t.loadFromFile("resources/block.png");
 	sf::Color wall_color = sf::Color::White;
@@ -59,23 +59,38 @@ Game make_game(int board_width = 10, int board_height = 20) {
 	return game;
 }
 
-bool move(Game& level, int x, int y) {
-	bool is_collision = false;
-	level.player_tetrimino.position.x += x * BLOCK_SCALE;
-	level.player_tetrimino.position.y += y * BLOCK_SCALE;
-
-	// collision check, real simple because blocks are
-	// grid and rotation locked, and the same size
+// collision check, real simple because blocks are
+// grid and rotation locked, and the same size
+bool check_collision(Game& level) {
+	bool is_collision = false;;
 	for (auto b : level.player_tetrimino.blocks) {
 		for (auto p : level.placed_blocks) {
 			if (p.getPosition() == b.getPosition()) {
-				level.player_tetrimino.position.x -= x * BLOCK_SCALE;
-				level.player_tetrimino.position.y -= y * BLOCK_SCALE;
 				is_collision = true;
 			}
 		}
 	}
-
 	return is_collision;
 }
 
+void move_player_tetrimino(Game& level, int x, int y) {
+	level.player_tetrimino.position.x += x * BLOCK_SCALE;
+	level.player_tetrimino.position.y += y * BLOCK_SCALE;
+
+	if (check_collision(level)) {
+		level.player_tetrimino.position.x -= x * BLOCK_SCALE;
+		level.player_tetrimino.position.y -= y * BLOCK_SCALE;
+	}
+}
+
+// swaps the player and held tetrimno
+// needs to do a check for when there is nothing held
+Tetrimino hold_tetrimino(Game& level);
+
+// returns true if a placed block is at the top
+bool is_game_over(Game& level);
+
+// pseudo random tetrimino generation
+// shuffles one of each block, and puts them in the queue
+void generate_tetrimino_batch(Game& level);
+void place_tetrimino(Game& level);
