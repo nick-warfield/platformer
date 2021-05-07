d972e5be63a3e3b6ee3ce893294d8249f46146ed
Nicholas Warfield <nick@nkwarfield.io>
2021-05-06 17:57:22 -0700

    Adjust Board Size The board size is actually a bit longer and their is no ceiling. Defeat is determined by going over a height of 20 blocks

--- include/Game.hpp
+++ include/Game.hpp
@@ -29,7 +29,7 @@ Game make_game(int board_width = 10, int board_height = 20) {
 	sf::Color wall_color = sf::Color::White;
 
 	// set walls
-	for (int i = 1; i < board_height; ++i) {
+	for (int i = 1; i < board_height + 10; ++i) {
 		sf::Sprite block1(t);
 		block1.setColor(wall_color);
 		block1.setPosition(0, i * BLOCK_SCALE);
@@ -42,18 +42,14 @@ Game make_game(int board_width = 10, int board_height = 20) {
 		game.placed_blocks.push_back(block2);
 	}
 
-	// set floor and ceiling
+	// set floor
 	for (int i = 0; i <= board_width; ++i) {
-		sf::Sprite block1(t);
-		block1.setColor(wall_color);
-		block1.setPosition(i * BLOCK_SCALE, 0);
-
-		sf::Sprite block2(t);
-		block2.setColor(wall_color);
-		block2.setPosition(i * BLOCK_SCALE, board_height * BLOCK_SCALE);
-
-		game.placed_blocks.push_back(block1);
-		game.placed_blocks.push_back(block2);
+		sf::Sprite block(t);
+		block.setColor(wall_color);
+		block.setPosition(
+				i * BLOCK_SCALE,
+				(board_height + 10) * BLOCK_SCALE);
+		game.placed_blocks.push_back(block);
 	}
 
 	return game;
